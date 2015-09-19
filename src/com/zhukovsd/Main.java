06742045d368dcd3e6919a3740b3fd80b0a4bcfa
zhukovsd <zhukovsd@gmail.com>
2015-09-19 23:21:49 +0300

    Main class added

---
+++ src/com/zhukovsd/Main.java
@@ -0,0 +1,14 @@
+package com.zhukovsd;
+
+/**
+ * Created by ZhukovSD on 19.09.2015.
+ */
+public class Main {
+    /**
+     * entry point
+     * @param args arguments from console
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
