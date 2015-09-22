3547bab1c43688f15a7c130dd948823aaed08ee8
zhukovsd <zhukovsd@gmail.com>
2015-09-22 17:14:55 +0300

    EmbeddedVertex.getPosition() returns Point2D.Double, calculateVertexPosition definition changed
    Vertex param in now type parameter <U extends Vertex> of GraphView<T, U>. It is useful on overrode methods to have vertex param typed in specific Node-derived class.

--- src/com/zhukovsd/graphs/embedded/EmbeddedVertex.java
+++ src/com/zhukovsd/graphs/embedded/EmbeddedVertex.java
@@ -1,7 +1,7 @@
 package com.zhukovsd.graphs.embedded;
 
 import com.zhukovsd.graphs.Vertex;
-import java.awt.*;
+import java.awt.geom.Point2D;
 
 /**
  * Abstract graph vertex, embedded to the coordinate space.
@@ -9,7 +9,7 @@ import java.awt.*;
 public abstract class EmbeddedVertex extends Vertex {
     /**
      * Retrieve position for current vertex in the coordinate space
-     * @return vertex position as {@link Point point} object
+     * @return vertex position as {@link Point2D.Double point} object
      */
-    protected abstract Point getPosition();
+    protected abstract Point2D.Double getPosition();
 }
