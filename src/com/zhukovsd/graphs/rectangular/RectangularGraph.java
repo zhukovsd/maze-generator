e8f3c301cd7f985be4ec412aea0e91396573a346
zhukovsd <zhukovsd@gmail.com>
2015-09-22 18:59:25 +0300

    RectangularGraph connects his nodes forming rectangular net of vertexes and edges

--- src/com/zhukovsd/graphs/rectangular/RectangularGraph.java
+++ src/com/zhukovsd/graphs/rectangular/RectangularGraph.java
@@ -1,5 +1,6 @@
 package com.zhukovsd.graphs.rectangular;
 
+import com.zhukovsd.graphs.Vertex;
 import com.zhukovsd.graphs.embedded.EmbeddedGraph;
 
 /**
@@ -17,7 +18,13 @@ public class RectangularGraph extends EmbeddedGraph<RectangularVertex> {
 
         for (int i = 0; i < rowCount; i++) {
             for (int j = 0; j < columnCount; j++) {
-                vertexList.add(new RectangularVertex(i, j));
+                RectangularVertex vertex = new RectangularVertex(i, j);
+
+                if (i > 0) vertex.connect(lattice[i-1][j], true);
+                if (j > 0) vertex.connect(lattice[i][j-1], true);
+
+                lattice[i][j] = vertex;
+                vertexList.add(vertex);
             }
         }
     }
