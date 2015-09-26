ba0f9c709f5cf848b9e503056d5d72e1a40dab34
zhukovsd <zhukovsd@gmail.com>
2015-09-26 16:05:39 +0300

    RectangularGraph() adapted for connect() method changes in impl-base-graph branch

--- src/com/zhukovsd/graphs/rectangular/RectangularGraph.java
+++ src/com/zhukovsd/graphs/rectangular/RectangularGraph.java
@@ -20,8 +20,8 @@ public class RectangularGraph extends EmbeddedGraph<RectangularVertex> {
             for (int j = 0; j < columnCount; j++) {
                 RectangularVertex vertex = new RectangularVertex(i, j);
 
-                if (i > 0) vertex.connect(lattice[i-1][j], true);
-                if (j > 0) vertex.connect(lattice[i][j-1], true);
+                if (i > 0) connect(vertex, lattice[i-1][j]);
+                if (j > 0) connect(vertex, lattice[i][j-1]);
 
                 lattice[i][j] = vertex;
                 vertexList.add(vertex);
