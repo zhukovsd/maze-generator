f16622b26501cd1d338b14c0d3adfe92050a65fc
zhukovsd <zhukovsd@gmail.com>
2015-09-26 16:02:03 +0300

    Method connect(), which connects 2 vertexes of graph, moved from Vertex class to Graph class
    This is done to provide ability to override connect() method and store some additional information about links between vertexes.

--- src/com/zhukovsd/graphs/Vertex.java
+++ src/com/zhukovsd/graphs/Vertex.java
@@ -11,16 +11,4 @@ public class Vertex {
      * List of {@link Edge edges}, directional connections between current vertex and adjacent to it
      */
     public final List<Edge> edgeList = new ArrayList<Edge>();
-
-    /**
-     * Method to connect current vertex to another vertex by creating edge(s)
-     * @param vertex vertex to connect to
-     * @param inBothDirections if true, 2 edges will be created, from <code>this</code> to <code>vertex</code>
-     * and vice versa, otherwise only one edge will be created in direction from current vertex
-     */
-    public void connect(Vertex vertex, boolean inBothDirections) {
-        edgeList.add(new Edge(this, vertex));
-
-        if (inBothDirections) vertex.edgeList.add(new Edge(vertex, this));
-    }
 }
