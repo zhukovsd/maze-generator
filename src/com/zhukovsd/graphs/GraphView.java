832f504799a806ee88cc82326ce904e63cf181a5
zhukovsd <zhukovsd@gmail.com>
2015-09-29 20:50:29 +0300

    Changed way to set paint style
    GraphView.color removed, color and stroke params added to GraphView.paint() method. Overload for GraphView.paint() added with default black color and 1 pixel width stroke.

--- src/com/zhukovsd/graphs/GraphView.java
+++ src/com/zhukovsd/graphs/GraphView.java
@@ -1,11 +1,9 @@
 package com.zhukovsd.graphs;
 
-import com.zhukovsd.graphs.embedded.EmbeddedVertex;
-
 import java.awt.*;
 
 /**
- * Representation of {@link Graph graph}, which can be painted on java.awt.Graphic object.
+ * Representation of {@link Graph graph}, which can be painted on java.awt.Graphic2D object.
  * @param <T> type of represented {@link Graph graph}
  * @param <U> type of {@link Vertex vertex} of represented {@link Graph graph}
  */
@@ -16,16 +14,15 @@ public abstract class GraphView<T extends Graph<U>, U extends Vertex> {
     private final T graph;
 
     /**
-     * Color which will be used for painting vertexes and edges of graph.
+     * Create graph representation of given {@link Graph graph}
+     * @param graph graph to represent
      */
-    public Color color;
-
     public GraphView(T graph) {
         this.graph = graph;
     }
 
     /**
-     * Getter for typed graph, may be overrode if graph have to be specific of specific type, derived from T
+     * Getter for typed graph, may be overrode if graph have to be specific of specific type, derived from T.
      * @return stored graph to represent
      */
     protected T getGraph() {
@@ -33,34 +30,34 @@ public abstract class GraphView<T extends Graph<U>, U extends Vertex> {
     }
 
     /**
-     * Abstract method for calculating size of image, representing current view. Must be overrode in derived class
+     * Abstract method for calculating size of image, representing current view. Must be overrode in derived class.
      * @return image size as {@link Point} object
      */
     public abstract Point getSize();
 
     /**
-     * Abstract method for calculating vertex position in current view representation. Must be overrode in derived class
+     * Abstract method for calculating vertex position in current view representation. Must be overrode in derived class.
      * @param vertex vertex to calculate position of
      * @return vertex position as {@link Point} object
      */
     protected abstract Point calculateVertexPosition(U vertex);
 
     /**
-     * Paint given vertex on given {@link Graphics graphics} object
-     * @param graphics {@link Graphics graphics} object to paint on
+     * Paint given vertex on given {@link Graphics2D graphics} object.
+     * @param graphics graphics object to paint on
      * @param vertex {@link Vertex vertex} object to paint
      */
-    protected void paintVertex(Graphics graphics, U vertex) {
+    protected void paintVertex(Graphics2D graphics, U vertex) {
         Point position = calculateVertexPosition(vertex);
         graphics.drawLine(position.x, position.y, position.x, position.y);
     }
 
     /**
-     * Paint given link on given {@link Graphics graphics} object
-     * @param graphics {@link Graphics graphics} object to paint on
+     * Paint given link on given {@link Graphics2D graphics} object.
+     * @param graphics graphics object to paint on
      * @param edge {@link Edge edge} object to paint
      */
-    protected void paintEdge(Graphics graphics, Edge edge) {
+    protected void paintEdge(Graphics2D graphics, Edge edge) {
         Point sourcePosition = calculateVertexPosition((U) edge.destination);
         Point destinationPosition = calculateVertexPosition((U) edge.source);
 
@@ -68,11 +65,14 @@ public abstract class GraphView<T extends Graph<U>, U extends Vertex> {
     }
 
     /**
-     * Paint whole view on given {@link Graphics graphics} object
-     * @param graphics {@link Graphics graphics} object to paint on
+     * Paint whole view on given {@link Graphics2D graphics} object with given color and stroke.
+     * @param graphics graphics object to paint on
+     * @param color color which will be used for painting
+     * @param stroke stroke which will be used for painting
      */
-    public void paint(Graphics graphics) {
+    public void paint(Graphics2D graphics, Color color, Stroke stroke) {
         graphics.setColor(color);
+        graphics.setStroke(stroke);
 
         for (U vertex : graph.vertexList) {
             paintVertex(graphics, vertex);
@@ -81,5 +81,13 @@ public abstract class GraphView<T extends Graph<U>, U extends Vertex> {
                 paintEdge(graphics, edge);
             }
         }
+    }
+
+    /**
+     * Paint whole view on given {@link Graphics2D graphics} object with black color and 1 pixel width stroke
+     * @param graphics graphics object to paint on
+     */
+    public void paint(Graphics2D graphics) {
+        paint(graphics, Color.BLACK, new BasicStroke(1));
     }
 }
