2c5d26df97d76ced693253217c8314bdb647648b
zhukovsd <zhukovsd@gmail.com>
2015-10-03 02:42:51 +0300

    Changes in graphs.dual package caused by switching to self-referential Vertex class
    Javadoc improvements.
    
    (cherry picked from commit f8ad6ceba088a35ba834ef8fe434a4ce3be96f24)

--- src/com/zhukovsd/graphs/dual/DualGraph.java
+++ src/com/zhukovsd/graphs/dual/DualGraph.java
@@ -1,5 +1,6 @@
 package com.zhukovsd.graphs.dual;
 
+import com.zhukovsd.graphs.Edge;
 import com.zhukovsd.graphs.Graph;
 import com.zhukovsd.graphs.embedded.EmbeddedGraph;
 import com.zhukovsd.graphs.embedded.EmbeddedVertex;
@@ -8,37 +9,38 @@ import com.zhukovsd.graphs.embedded.Face;
 import java.util.HashMap;
 
 /**
- * Every vertex of dual graph lays inside of corresponding face of outer graph. In contrast to classical dual graph
- * in graph theory, this implementation does not have vertex for outer face of outer graph. Outer graph must be embedded
- * in order to have faces, <a href="https://en.wikipedia.org/wiki/Dual_graph">Dual graph on wikipedia</a>.
+ * Every vertex of dual graph lays inside of corresponding {@link Face face} of outer graph. In contrast to classical dual graph
+ * in graph theory, this implementation does not have vertex for outer face of outer graph.
+ * Outer graph must be {@link EmbeddedGraph embedded} in order to have faces.
+ * <a href="https://en.wikipedia.org/wiki/Dual_graph">Dual graph on wikipedia</a>.
  */
-public class DualGraph<T extends EmbeddedVertex> extends Graph<DualVertex> {
+public class DualGraph<E extends EmbeddedVertex<E>> extends Graph<DualVertex<E>> {
     /**
-     * Outer graph of current dual graph.
+     * Outer {@link EmbeddedGraph embedded graph} of current dual graph.
      */
-    private EmbeddedGraph<T> outerGraph;
+    private EmbeddedGraph<E> outerGraph;
 
     /**
-     * Create and fill graph. Create vertex for every outer face in outer graph, create 2 directional edges for
-     * every pair of adjacent faces in outer graph
+     * Create and fill graph. Create {@link DualVertex vertex} for every outer {@link Face face} in outer graph,
+     * create 2 directional {@link Edge edges} for every pair of adjacent faces in outer graph.
      */
-    public DualGraph(EmbeddedGraph<T> outerGraph) {
+    public DualGraph(EmbeddedGraph<E> outerGraph) {
         this.outerGraph = outerGraph;
 
         // maps faces to vertexes to connect nodes according by outer graph's faces adjacency relations
-        HashMap<Face, DualVertex> map = new HashMap<Face, DualVertex>();
+        HashMap<Face<E>, DualVertex<E>> map = new HashMap<>();
 
         // first, create all vertexes
-        for (Face face : outerGraph.getFaces()) {
-            DualVertex vertex = new DualVertex(face);
+        for (Face<E> face : outerGraph.getFaces()) {
+            DualVertex<E> vertex = new DualVertex<>(face);
 
             vertexList.add(vertex);
             map.put(face, vertex);
         }
 
         // second, connect them
-        for (Face face : outerGraph.getFaces()) {
-            for (Face adjacentFace : face.adjacentFaceList) {
+        for (Face<E> face : outerGraph.getFaces()) {
+            for (Face<E> adjacentFace : face.adjacentFaceList) {
                 connect(map.get(face), map.get(adjacentFace));
             }
         }
