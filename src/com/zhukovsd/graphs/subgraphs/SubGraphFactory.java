a5783256cb909cd2be36393a34157c50d858deeb
zhukovsd <zhukovsd@gmail.com>
2015-10-04 00:04:25 +0300

    Changes in graphs.subgraphs package caused by switching to self-referential Vertex class
    Javadoc improvements.

--- src/com/zhukovsd/graphs/subgraphs/SubGraphFactory.java
+++ src/com/zhukovsd/graphs/subgraphs/SubGraphFactory.java
@@ -5,25 +5,39 @@ import com.zhukovsd.graphs.Graph;
 import com.zhukovsd.graphs.Vertex;
 
 /**
- * Created by ZhukovSD on 30.09.2015.
+ * Factory useful for instantiating specific {@link SubGraph subgraphs} such as spanning tree.
  */
 public class SubGraphFactory {
-    public static <T extends Vertex> SubGraph<T> createSpanningTree(Graph<T> parentGraph) {
-        SubGraph<T> result = new SubGraph<T>(parentGraph);
+    /**
+     * create spanning tree for given graph, spanning tree is a subgraph of given {@link Graph graph},
+     * which contains all of it's {@link Vertex vertexes} without loops.
+     * @param parentGraph parent graph
+     * @param <T> type of vertex in parent graph
+     * @return resulting spanning tree
+     */
+    public static <T extends Vertex<T>> SubGraph<T> createSpanningTree(Graph<T> parentGraph) {
+        SubGraph<T> result = new SubGraph<>(parentGraph);
         result.findSpanningTree();
 
         return result;
     }
 
-    public static <T extends Vertex> SubGraph<T> createFullSubGraph(Graph<T> parentGraph) {
-        SubGraph<T> result = new SubGraph<T>(parentGraph);
+    /**
+     * create full subgraph for given {@link Graph graph}, which means that it's contains all of it's {@link Vertex vertexes}
+     * and {@link Edge edges}.
+     * @param parentGraph parent graph
+     * @param <T> type of vertex in parent graph
+     * @return resulting subgraph
+     */
+    public static <T extends Vertex<T>> SubGraph<T> createFullSubGraph(Graph<T> parentGraph) {
+        SubGraph<T> result = new SubGraph<>(parentGraph);
 
-        for (Vertex vertex : parentGraph.vertexList) {
-            result.addVertex(new SubGraphVertex(vertex));
+        for (T vertex : parentGraph.vertexList) {
+            result.addVertex(new SubGraphVertex<>(vertex));
         }
 
-        for (Vertex vertex : parentGraph.vertexList) {
-            for (Edge edge : vertex.edgeList) {
+        for (T vertex : parentGraph.vertexList) {
+            for (Edge<T> edge : vertex.edgeList) {
                 result.connect(result.parentVertexesMap.get(edge.source), result.parentVertexesMap.get(edge.destination));
             }
         }
