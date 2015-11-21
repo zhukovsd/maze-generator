9a2cecca1d23c825252366af80bd8f19cbb99399
zhukovsd <zhukovsd@gmail.com>
2015-11-21 15:15:09 +0300

    MIN_VERTEX_INTERVAL declared in HexahedralGraphView
    Better javadoc provided.

--- src/com/zhukovsd/graphs/hexahedral/HexahedralGraphView.java
+++ src/com/zhukovsd/graphs/hexahedral/HexahedralGraphView.java
@@ -8,9 +8,14 @@ import java.awt.*;
 import static java.lang.Math.*;
 
 /**
- * Created by ZhukovSD on 15.11.2015.
+ * View of graph in form of hexahedral lattice.
  */
 public class HexahedralGraphView extends EmbeddedGraphView<HexahedralVertex> {
+    /**
+     * Minimal node spacing for hexahedral graph view.
+     */
+    static final int MIN_VERTEX_INTERVAL = 5;
+
     /**
      * Outer indent from border of the view to the most outer vertex.
      */
@@ -21,6 +26,12 @@ public class HexahedralGraphView extends EmbeddedGraphView<HexahedralVertex> {
      */
     private int vertexInterval;
 
+    /**
+     * Create hexahedral graph view for given {@link HexahedralGraph graph} and given size parameters
+     * @param graph hexahedral graph
+     * @param indent indent
+     * @param vertexInterval vertex interval
+     */
     public HexahedralGraphView(EmbeddedGraph<HexahedralVertex> graph, int indent, int vertexInterval) {
         super(graph);
 
@@ -28,11 +39,19 @@ public class HexahedralGraphView extends EmbeddedGraphView<HexahedralVertex> {
         this.vertexInterval = vertexInterval;
     }
 
+    /**
+     * Get represented graph casted to HexahedralGraph type.
+     * @return typed graph
+     */
     @Override
     protected HexahedralGraph getGraph() {
         return ((HexahedralGraph) super.getGraph());
     }
 
+    /**
+     * Method for calculating size of image, representing current view.
+     * @return image size as {@link Point} object
+     */
     @Override
     public Point getSize() {
         int halfCount = ((getGraph().latticeColumnCount + 1) / 2);
@@ -44,14 +63,23 @@ public class HexahedralGraphView extends EmbeddedGraphView<HexahedralVertex> {
         );
     }
 
+    /**
+     * Method for adjusting current view width by changing its size vertex interval.
+     * @param availableWidth width to adjust to
+     */
     @Override
     public void adjustSizeByWidth(int availableWidth) {
         if (getSize().x > availableWidth) {
             vertexInterval = ((int) ((availableWidth - 2 * indent) / (0.75 * (getGraph().latticeColumnCount - 1))));
-            if (vertexInterval < 4) vertexInterval = 4;
+            if (vertexInterval < MIN_VERTEX_INTERVAL) vertexInterval = MIN_VERTEX_INTERVAL;
         }
     }
 
+    /**
+     * Method for calculating vertex position in current view representation.
+     * @param vertex vertex to calculate position of
+     * @return vertex position as {@link Point} object
+     */
     @Override
     public Point calculateVertexPosition(HexahedralVertex vertex) {
         int wholeCount = (vertex.latticeColumnIndex / 2);
